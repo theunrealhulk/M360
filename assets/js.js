60b876a84fe633def906b2bac46df86e25546d5d
theunrealhulk <chafiaay.abj@gmail.com>
2024-01-16 22:10:45 +0100

    setup player

---
+++ assets/js.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+// load config file
+var settings={}
+    $.get("/assets/config.json", function(data, status){
+        console.log("Data: " + data + "\nStatus: " + status);
+
+        if(status=="success"){
+            settings =(data)
+            console.log("Settings: " + settings.numberOfLevels )
+        }
+        else{
+            console.log("Error: loading Config File" + status)
+        }
+    });
+});
